281e22577fc883c437c05073462e593ea794741b
theocita <theocitaallen@gmail.com>
2022-10-15 16:27:38 -0400

    Complete Assigment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+let element = document.querySelector('main#main');
+element.remove();
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = 'Theo is the champion'
